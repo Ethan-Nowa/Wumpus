d3ce7eca99b767a90a354eedf5a1b0e89648c67f
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-01-25 01:07:28 -0500

    Update Main.java
    Moved to Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,22 @@
+import static javax.swing.SwingUtilities.invokeLater;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
 
 public class Main {
+
+	public static void main(String[] args) {
+	    invokeLater(() -> {
+	        JFrame f = new JFrame();
+	        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        f.setTitle("Hunt The Wumpus");
+	        f.setResizable(false);
+	        f.add(new Wumpus(), BorderLayout.CENTER);
+	        f.pack();
+	        f.setLocationRelativeTo(null);
+	        f.setVisible(true);
+	    });
+	}
 
 }
